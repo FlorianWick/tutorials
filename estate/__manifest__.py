bd26b073ddd9941b8827e305aa19a928972e1948
unknown <fl.wick96@gmail.com>
2024-10-14 22:40:17 +0200

    chapter5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,6 +2,7 @@
 'name': 'Estate',
 'data': [
     'security/ir.model.access.csv',
-    'views/estate_property_views.xml'
+    'views/estate_property_views.xml',
+    'views/estate_menus.xml',
     ]
 }
